697ea0aff0ce63ad3ed007339299663c84426e75
JJ Lee <comseong@gmail.com>
2015-09-20 13:36:56 +0500

    Codes Refactoring and Added file writing function.

--- generate_geojson.php
+++ generate_geojson.php
@@ -6,12 +6,15 @@
     //generate_ByCategroy("http://localhost", "Food%26Drink");
 
 
-    function generate_ByCategroy($domain, $category){
-        // Curl JSON data from Food&Drink Category
-        $PageInfoQuery = $domain . "/api.php?action=query&prop=revisions&rvprop=content&format=json&generator=categorymembers&gcmtitle=Category:" . $category;
+    function generate_ByCategroy($url, $category){
+        // Curl JSON data from Category
+        $PageInfoQuery = $url . "/api.php?action=query&prop=revisions&rvprop=content&format=json&generator=categorymembers&gcmtitle=Category:" . $category;
         $result= curl_http_get($PageInfoQuery);
 
-        parse_queryData($domain, $result);
+        $propertiesArray = parse_queryData($url, $result);
+        $featureElements = encode_geojson_features($propertiesArray);
+        $geojson = encode_geojson_FeatureCollection($featureElements);
+        write_file_inDownloadDir($url, $category, $geojson);
     }
 
 
@@ -28,7 +31,8 @@
     }
 
 
-    function parse_queryData($domain, $result){
+    function parse_queryData($url, $result){
+
         // Parse the JSON format of  "DataInputForm Template"
         $data = json_decode($result);
         //print_r($data);
@@ -44,7 +48,7 @@
             $temp = explode('|', $dataInputForm);
             //print_r($temp);
 
-            $photoPath = find_imagePath($domain, $pageTitle);
+            $photoPath = find_imagePath($url, $pageTitle);
             $rating = find_rating($pageId);
             $name = $pageTitle;
             $desc = trim(explode('=',$temp[8])[1]);
@@ -58,20 +62,42 @@
             $gps = trim(explode('=',$temp[16])[1]);
             $gps = trim(explode("}}", $gps)[0]); // Remove "}}" from $gps
 
-            // Encode to GeoJSON features
-            $features = encode_geojson_features($photoPath, $rating, $name, $desc, $type, $price, $wifi, $open, $addr, $tel, $url, $gps);
+            $properties['photoExt'] = strrchr($photoPath, '.');
+            $encoded_image = encode_ImageToBase64($photoPath);
+            $properties['photo'] = $encoded_image;
+            $properties['rating'] = $rating;
+            $properties['name'] = $name;
+            $properties['desc'] = $desc;
+            $properties['type'] = $type;
+            $properties['price'] = $price;
+            $properties['wifi'] = $wifi;
+            $properties['open'] = $open;
+            $properties['addr'] = $addr;
+            $properties['tel'] = $tel;
+            $properties['url'] = $url;
+
+            //Decompose $gps for Point format
+            $lat = $long = 0;
+            if($gps != null) {
+                $fields = explode(",", $gps);
+                if ($fields[0] != null) {
+                    $long = (float)trim($fields[0]);
+                }
+                if ($fields[1] != null) {
+                    $lat = (float)trim($fields[1]);
+                }
+            }
+
+            $featureElements[] = array('properties'=>$properties, 'lat'=>$lat, 'long'=>$long);
         }
 
-        // Encode to GeoJSON featurecollcection and Save it to the file in download folder
-        $featurecollection = new \GeoJson\Feature\FeatureCollection($features);
-        $geojson = json_encode($featurecollection);
-        print_r($geojson);
+        return $featureElements;
     }
 
 
-    function find_imagePath($domain, $title) {
+    function find_imagePath($url, $title) {
         // Create a image of size of 528px
-        $ImageCreateQuery = $domain . "/api.php?action=query&prop=pageimages&format=json&piprop=thumbnail&pithumbsize=528&generator=images&titles=" . $title;
+        $ImageCreateQuery = $url . "/api.php?action=query&prop=pageimages&format=json&piprop=thumbnail&pithumbsize=528&generator=images&titles=" . $title;
         $result = curl_http_get($ImageCreateQuery);
 
         //Parse JSON and Extract ImagePath
@@ -89,49 +115,73 @@
         return $imagePath;
     }
 
-    function find_rating($pageId){
-        $rating = 4;
-        return $rating;
-    }
-
-    function encode_geojson_features($photoPath, $rating, $name, $desc, $type, $price, $wifi, $open, $addr, $tel, $url, $gps){
-        static $features;
-
-        $encoded_image = encode_ImageToBase64($photoPath);
-        $properties['photo'] = $encoded_image;
-        $properties['rating'] = $rating;
-        $properties['name'] = $name;
-        $properties['desc'] = $desc;
-        $properties['type'] = $type;
-        $properties['price'] = $price;
-        $properties['wifi'] = $wifi;
-        $properties['open'] = $open;
-        $properties['addr'] = $addr;
-        $properties['tel'] = $tel;
-        $properties['url'] = $url;
-
-        //Decompose $gps for Point format
-        $lat = $long = 0;
-        if($gps != null) {
-            $fields = explode(",", $gps);
-            if ($fields[0] != null) {
-                $long = (float)trim($fields[0]);
-            }
-            if ($fields[1] != null) {
-                $lat = (float)trim($fields[1]);
-            }
-        }
-        // Generate GeoJSON format
-        $point = new \GeoJson\Geometry\Point([$lat, $long]);
-        $features[] = new \GeoJson\Feature\Feature($point, $properties, null);
-
-        return $features;
-    }
 
     function encode_ImageToBase64($imagePath){
         $image_data=file_get_contents($imagePath);
         $encoded_image=base64_encode($image_data);
 
         return $encoded_image;
+    }
+
+
+    function find_rating($pageId){
+        $rating = 4;
+        return $rating;
+    }
+
+
+    function encode_geojson_features($featureElements){
+
+        $count = count($featureElements);
+        for ($i = 0; $i < $count; $i++) {
+            // Generate GeoJSON format
+            $point = new \GeoJson\Geometry\Point([$featureElements[$i]['lat'], $featureElements[$i]['long']]);
+            $features[] = new \GeoJson\Feature\Feature($point, $featureElements[$i]['properties'], null);
+        }
+
+        return $features;
+    }
+
+
+    function encode_geojson_FeatureCollection($features){
+        // Encode to GeoJSON featurecollcection and Save it to the file in download folder
+        $featurecollection = new \GeoJson\Feature\FeatureCollection($features);
+        $geojson = json_encode($featurecollection);
+
+        return $geojson;
+
+    }
+
+
+    function write_file_inDownloadDir($url, $category, $geojson){
+        $host = parse_url($url)['host'];
+        $subdomain = explode('.', $host)[0];
+
+        // Unification of category name
+        if($category == "Ovqatlanish" || $category == "Food%26Drink"){
+            $category = "foodndriank";
+        }
+        elseif($category == "Mehmoxonalar" || $category == "Hotel"){
+            $category = "hotel";
+        }
+        elseif($category == "Ziyoratgohlar" || $category == "Attraction"){
+            $category = "attraction";
+        }
+        elseif($category == "Xaridlar" || $category == "Shopping"){
+            $category = "shopping";
+        }
+
+        $dtz = new DateTimeZone("Asia/Samarkand");
+        $current = new DateTime("now", $dtz);
+        $date = $current->format("Ymd");
+
+        $filename = "download/" . $subdomain . '_' . $category . '_' . $date . ".geojson";
+        $file_handle = fopen($filename, "w+");
+        if (fwrite($file_handle, $geojson) == FALSE) {
+            echo "Cannot write to file ($filename)";
+        }
+        fclose($file_handle);
+
+        echo "Generated $filename";
     }
 ?>
